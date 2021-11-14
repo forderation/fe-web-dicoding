70146979e2a28afee9fce9d04d0ebabca557eec3
kharismamuzaki <kharisma.ghufron@tokopedia.com>
2021-11-14 21:09:01 +0700

    fix: fixing sw

--- src/scripts/views/components/restaurant-item.js
+++ src/scripts/views/components/restaurant-item.js
@@ -13,21 +13,23 @@ export default class RestaturantItem extends HTMLElement {
   render () {
     const restaturant = this._restaturant;
     this.innerHTML = /* html */ `
-    <div class="card food-card pointer">
-        <section class="food-header">
-            <div class="image-card">
-                <img src="${API_ENDPOINT.IMAGE(restaturant.pictureId)}" alt="${restaturant.name}" />
-                <div class="label-city">
-                    <p>${restaturant.city}</p>
-                </div>
-            </div>
-        </section>
-        <section class="food-body">
-            <i class="fas fa-star fa-lg"></i> ${restaturant.rating}
-            <h1 class="food-title">${restaturant.name}</h1>
-            <p class="food-description">${restaturant.description}</p>
-        </section>
-    </div>
+      <div class="card food-card pointer">
+          <section class="food-header">
+              <div class="image-card">
+                  <img src="${API_ENDPOINT.IMAGE(restaturant.pictureId)}" alt="${restaturant.name}" />
+                  <div class="label-city">
+                      <p>${restaturant.city}</p>
+                  </div>
+              </div>
+          </section>
+          <section class="food-body">
+              <i class="fas fa-star fa-lg"></i> ${restaturant.rating}
+              <div class="food-title">
+                <a href="${API_ENDPOINT.DETAIL_ROUTE(restaturant.id)}">${restaturant.name}</a>
+              </div>
+              <p class="food-description">${restaturant.description}</p>
+          </section>
+      </div>
     `;
     this.addEventListener('click', function () {
       window.location.hash = API_ENDPOINT.DETAIL_ROUTE(restaturant.id);
