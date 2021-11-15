e8be2bd610f26b007ad5b08d8cd5cd274eb90b38
kharismamuzaki <kharisma.ghufron@tokopedia.com>
2021-11-15 20:07:42 +0700

    feat: revamp callback detail

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -11,25 +11,25 @@ const Home = {
       <div id="error-section">
       </div>
       <div class="top-banner" id="banner-food">
-        <h1 id="banner-title">What we serve</h1>
+        <h1 id="banner-title" tabindex="0">What we serve</h1>
         <div class="services">
           <section class="service">
             <img src="./images/img1.png" alt="Easy To Order" />
-            <figcaption>Easy To Order</figcaption>
-            <p>You only need a few steps in ordering food.</p>
+            <figcaption tabindex="0">Easy To Order</figcaption>
+            <p tabindex="0">You only need a few steps in ordering food.</p>
           </section>
           <section class="service">
             <img src="./images/img2.png" alt="Fastest Delivery" />
-            <figcaption>Fastest Delivery</figcaption>
-            <p>Delivery that is always ontime even faster.</p>
+            <figcaption tabindex="0">Fastest Delivery</figcaption>
+            <p tabindex="0">Delivery that is always ontime even faster.</p>
           </section>
           <section class="service">
             <img src="./images/img3.png" alt="Best Quality" />
-            <figcaption>Best Quality</figcaption>
-            <p>Not only fast for us quality is also number one.</p>
+            <figcaption tabindex="0">Best Quality</figcaption>
+            <p tabindex="0">Not only fast for us quality is also number one.</p>
           </section>
         </div>
-        <h1 id="banner-desc">Explore Your Foods</h1>
+        <h1 id="banner-desc" tabindex="0">Explore Your Foods</h1>
       </div>
       <div id="contents" class="explore-foods"></div>
     `;
