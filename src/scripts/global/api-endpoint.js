434d258ee11c6bbefde0e278325888be6968ecdf
kharismamuzaki <kharisma.ghufron@tokopedia.com>
2021-12-07 23:51:37 +0700

    search bar home

--- src/scripts/global/api-endpoint.js
+++ src/scripts/global/api-endpoint.js
@@ -3,6 +3,7 @@ import CONFIG from './config';
 const API_ENDPOINT = {
   LIST: `${CONFIG.BASE_URL}list`,
   DETAIL: (id) => `${CONFIG.BASE_URL}detail/${id}`,
+  SEARCH: (query) => `${CONFIG.BASE_URL}search?q=${query}`,
   IMAGE: (pictureId) => `${CONFIG.BASE_IMAGE_URL}${pictureId}`,
   DETAIL_ROUTE: (id) => `#/detail/${id}`,
   SUBMIT_REVIEW: `${CONFIG.BASE_URL}review`
